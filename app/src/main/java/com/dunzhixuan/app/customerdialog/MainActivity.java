7b44b636783fbd6108db4b9130dc2ea1ab336db5
dunzhixuan <dunzhixuan@vipkid.com.cn>
2019-04-16 11:58:44 +0800

    feat: 测试推送

--- app/src/main/java/com/dunzhixuan/app/customerdialog/MainActivity.java
+++ app/src/main/java/com/dunzhixuan/app/customerdialog/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 		protected void onCreate(Bundle savedInstanceState) {
 				super.onCreate(savedInstanceState);
 
+				//测试推送
 				showAlertDialog();
 		}
 
